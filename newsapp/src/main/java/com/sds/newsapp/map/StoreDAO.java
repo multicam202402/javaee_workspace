0a867dddf818ff47e3bedcc0d65a1f07f5ab5651
multicampus202402 <multicam202402@gmail.com>
2024-03-22 17:40:19 +0900

    금일수업

--- newsapp/src/main/java/com/sds/newsapp/map/StoreDAO.java
+++ newsapp/src/main/java/com/sds/newsapp/map/StoreDAO.java
@@ -1,14 +1,60 @@
 package com.sds.newsapp.map;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 //오직 store 테이블에 대한 CRUD만을 담당하는 객체인 DAO 정의 
 public class StoreDAO {
-	
+	String driver="oracle.jdbc.driver.OracleDriver";
+	String url="jdbc:oracle:thin:@localhost:1521:XE";
+	String user="seshop";
+	String pass="1234";	
 	
 	//등록
-	public int insert() {
-		return 0;
+	public int insert(Store store) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		int result=0; //쿼리 수행 결과를 담게 될 변수 
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, pass);
+			
+			String sql="insert into store(store_idx, name, lati, longi) values(seq_store.nextval,?,?,?)";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setString(1, store.getName());
+			pstmt.setDouble(2, store.getLati());
+			pstmt.setDouble(3, store.getLongi());
+			
+			result = pstmt.executeUpdate(); //쿼리 실행
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt !=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con !=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		
+		
+		return result;
 	}
 		
 	//목록
--- newsapp/src/main/webapp/map/regist.jsp
+++ newsapp/src/main/webapp/map/regist.jsp
@@ -1,4 +1,10 @@
+<%@page import="com.sds.newsapp.map.StoreDAO"%>
+<%@page import="com.sds.newsapp.map.Store"%>
 <%@ page contentType="text/html;charset=UTF-8"%>
+<%! 
+	//이 jsp 가 서블릿으로 변경될때, 멤버 영역
+	StoreDAO storeDAO = new StoreDAO();
+%>
 <%
 	//클라이언트가 비동기 요청을 시도한다는 것은, 현재 클라이언트가 보고 있는 페이지를 새로고침 하기를
 	//원하지 않는다는 것이다. 따라서 서버에서는 완전한 html문서로 응답을 해서는 안됌!!
@@ -10,9 +16,28 @@
 	String lati=request.getParameter("lati");
 	String longi=request.getParameter("longi");
 	String name=request.getParameter("name");
+	Store store = new Store(); //empty
 	
-	out.print("lati="+lati+", longi="+longi+", name="+name);
+	//비어있는 store 채우기 (setter 로 채우자)
+	store.setName(name);
+	store.setLati(Double.parseDouble(lati));
+	store.setLongi(Double.parseDouble(longi));
 	
+	int result = storeDAO.insert(store);
+	
+	StringBuilder sb = new StringBuilder();
+	
+	sb.append("{");
+	if(result>0){
+		sb.append("\"result\":\"ok\"");
+	}else{
+		sb.append("\"result\":\"fail\"");
+	}
+	sb.append("}");
+	
+	out.print(sb.toString()); //결과를 json문자열로 보낸다...
+	
+	//out.print("lati="+lati+", longi="+longi+", name="+name);
 %>
 
 
