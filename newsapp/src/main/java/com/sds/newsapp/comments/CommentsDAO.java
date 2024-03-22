80a2451b19f9502e5d7ad810f36417f7c388ff6b
multicampus202402 <multicam202402@gmail.com>
2024-03-22 13:35:10 +0900

    금일수업

--- newsapp/src/main/java/com/sds/newsapp/comments/CommentsDAO.java
+++ newsapp/src/main/java/com/sds/newsapp/comments/CommentsDAO.java
@@ -3,7 +3,10 @@ package com.sds.newsapp.comments;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 //이 클래스는, 오직 데이터베이스 테이블에 대한 CRUD 만을 수행하는 로직만 들어있어야 한다..
 //이렇게 데이터베이스에 대한 CRUD만을 수행하는 객체를 가리켜 어플리케이션 설계 분야에서는 DAO
@@ -66,10 +69,65 @@ public class CommentsDAO {
 	public void selectAllByFkey(int news_idx) {
 		Connection con=null;
 		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List list=new ArrayList(); //Commnets DTO 의 인스턴스들을 모아놓을 컬렉션	
+											//예) 게시물이 5개라면 DTO의 인스턴스도 5개를 생성하여 이 컬렉션에 넣자
 		
+		try {
+			Class.forName(driver);
+			con=DriverManager.getConnection(url, user, pass);
+			
+			String sql="select * comments where news_idx=? order by desc";
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, news_idx); 
+			rs=pstmt.executeQuery(); //쿼리수행 후 결과 반환
+			
+			while(rs.next()) { //커서 한칸 전진 
+				//rs에 들어있는 컬럼의 값들을 DTO의 멤버변수로 옮긴다
+				Comments comments = new Comments(); //비어있는 dto 생성 
+				comments.setComments_idx(rs.getInt("comments_idx"));
+				comments.setMsg(rs.getString("msg"));
+				comments.setCwriter(rs.getString("cwriter"));
+				comments.setCregdate(rs.getString("cregdate"));
+				comments.setNews_idx(rs.getInt("news_idx"));
+				
+				//생성된 DTO를 list에 담자 
+				list.add(comments);			
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			
+			
+			
+			
+		}
 		
-		String sql="select * comments where news_idx=? order by desc";
-		
+
 	}
 }
 
