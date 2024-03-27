bfb07edab515e7a9673fb7cdcbbf963004575923
multicampus202402 <multicam202402@gmail.com>
2024-03-27 15:02:07 +0900

    금일수업

--- AsyncBoard/src/main/java/com/sds/asynboard/board/BoardDAO.java
+++ AsyncBoard/src/main/java/com/sds/asynboard/board/BoardDAO.java
@@ -2,13 +2,54 @@ package com.sds.asynboard.board;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.sds.asynboard.common.PoolManager;
 
 //이 클래스는, 오직  board 테이블에 대한 CRUD를 수행하기 위한 객체이다 
 public class BoardDAO {
 	PoolManager pool=new PoolManager(); //수영장 관리 객체 생성
+	
+	//모든 글 가져오기 
+	public List selectAll() {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List list = new ArrayList();
+		
+		con = pool.getConnection(); //접속이 아니라, 이미 접속되어 있는 Connection 대여하기!!
+		
+		String sql="select * from board order by board_idx desc"; //내림차순
+		try {
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery(); //쿼리실행 후 표 반환
+			
+			//rs를 대신할 자료형을 대체...List<Board> 형태가 그 대안 임..
+			while(rs.next()) {
+				Board board = new Board(); //empty 상태 
+				//비어 있는 Board에 rs의 컬럼 값들을 채워넣기!!!
+				board.setBoard_idx(rs.getInt("board_idx"));
+				board.setTitle(rs.getString("title"));
+				board.setWriter(rs.getString("writer"));
+				board.setContent(rs.getString("content"));
+				board.setRegdate(rs.getString("regdate"));
+				board.setHit(rs.getInt("hit"));
+				
+				list.add(board);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			pool.release(con, pstmt, rs);
+		}
+		
+	
+		return list;
+	}
+	
 	
 	//글 1건 등록 
 	public int insert(Board board) {
--- AsyncBoard/src/main/java/com/sds/asynboard/board/ListServlet.java
+++ AsyncBoard/src/main/java/com/sds/asynboard/board/ListServlet.java
@@ -12,6 +12,10 @@ public class ListServlet extends HttpServlet{
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("목록 요청 들어왔어!!");
+		
+		//데이터베이스 목록을 가져와서, 클라이언트인 js가 이해할 수 잇는 데이터 형태로 변환하여 응답정보로
+		//보내주자...즉 json 으로 보내주자..
+		 
 	}
 }
 
