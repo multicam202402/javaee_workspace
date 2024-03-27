28baf5a3ec4c37c1fd933622a08a9ca40527e4a2
multicampus202402 <multicam202402@gmail.com>
2024-03-27 16:33:06 +0900

    금일수업

--- AsyncBoard/src/main/java/com/sds/asynboard/board/BoardDAO.java
+++ AsyncBoard/src/main/java/com/sds/asynboard/board/BoardDAO.java
@@ -51,6 +51,39 @@ public class BoardDAO {
 	}
 	
 	
+	//글 한건 가져오기 , 만일 일치하는 데이터가 없을 경우  Board는  null 이 반환.. 
+	//따라서 이 메서드 호출자는 널인지 아닌지에 따라 데이터가 있다 없다를 판단하면 된다..
+	public Board select(int board_idx) {
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		Board board=null;
+		
+		con=pool.getConnection(); //접속 객체 대여
+		String sql="select * from board where board_idx=?";
+		try {
+			pstmt=con.prepareStatement(sql);
+			pstmt.setInt(1, board_idx);
+			rs=pstmt.executeQuery(); //쿼리문 실행 후, 레코드 1건 가져오기 
+			if(rs.next()) { //레코드가 존재한다면..
+				board = new Board(); //empty 상태
+				board.setBoard_idx(rs.getInt("board_idx"));
+				board.setTitle(rs.getString("title"));
+				board.setWriter(rs.getString("writer"));
+				board.setContent(rs.getString("content"));
+				board.setRegdate(rs.getString("regdate"));
+				board.setHit(rs.getInt("hit"));
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			pool.release(con, pstmt, rs);
+		}
+		return board;		
+	}
+	
+	
 	//글 1건 등록 
 	public int insert(Board board) {
 		Connection con=null;
